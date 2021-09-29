05e7bce0ef5725d6ef14134178c64dc6df6b4d5b
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2021-09-29 16:31:58 +0700

    fixed

--- __clean_folder.bat
+++ __clean_folder.bat
@@ -1,11 +1,11 @@
-if %1 == "" (
+if [%1]==[] (
     echo "Не указан путь"
     goto EXIT)
 
 for %%I in (%1\*) do (
     if %%~zI==0 (
         echo found: %%I
-        rem del "%%I"
+        del "%%I"
     )
 )
 
