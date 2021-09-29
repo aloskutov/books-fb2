fd9fb9bb0c052bdfd1d8629ccdcf5c89a0375f88
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2021-09-29 16:41:23 +0700

    added custom work folder

--- books-clean.bat
+++ books-clean.bat
@@ -1,9 +1,14 @@
 @echo off
-set books_dir="e:\test\"
-echo Delete .null-sized files
+if [%1] == [] (
+    set books_dir="e:\test"
+) else (
+    set books_dir=%1
+)
+
+echo Delete .null-sized zip-files
+echo Work folder: %books_dir%
 
 call __clean_folder %books_dir%
-
 for /R %books_dir% /d %%B in (*) do (
     call __clean_folder "%%B"
 )
--- books-pack.bat
+++ books-pack.bat
@@ -1,6 +1,12 @@
 @echo off
-set books_dir="e:\test\"
+if [%1] == [] (
+    set books_dir="e:\test"
+) else (
+    set books_dir=%1
+)
+
 echo Pack fb2-file to fb2.zip-file
+echo Work folder: %books_dir%
 
 call __pack_folder %books_dir%
 
